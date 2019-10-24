bdf316f5ad79d31b9755087a3c5d7532d9b9ccf7
Florian Lorétan <florian.loretan@wunder.io>
2019-10-24 11:46:38 +0200

    Only login to the cluster if needed.

--- delete-deployment.sh
+++ delete-deployment.sh
@@ -1,14 +1,19 @@
-# Save key, authenticate and set compute zone
-echo $GCLOUD_KEY_JSON > ${HOME}/gcloud-service-key.json
-echo "Logging on to Google Cloud"
-gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json --project $GCLOUD_PROJECT_NAME
-gcloud config set compute/zone $GCLOUD_COMPUTE_ZONE
 
-# Updates a kubeconfig file with appropriate credentials and endpoint information 
-# to point kubectl at a specific cluster in Google Kubernetes Engine
-gcloud container clusters get-credentials $GCLOUD_CLUSTER_NAME \
-  --zone $GCLOUD_COMPUTE_ZONE \
-  --project $GCLOUD_PROJECT_NAME
+kubectl version --short
+
+if [[ $? -ne 0 ]]; then
+  # Save key, authenticate and set compute zone
+  echo $GCLOUD_KEY_JSON > ${HOME}/gcloud-service-key.json
+  echo "Logging on to Google Cloud"
+  gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json --project $GCLOUD_PROJECT_NAME
+  gcloud config set compute/zone $GCLOUD_COMPUTE_ZONE
+
+  # Updates a kubeconfig file with appropriate credentials and endpoint information
+  # to point kubectl at a specific cluster in Google Kubernetes Engine
+  gcloud container clusters get-credentials $GCLOUD_CLUSTER_NAME \
+    --zone $GCLOUD_COMPUTE_ZONE \
+    --project $GCLOUD_PROJECT_NAME
+fi
 
 # Remove deployment
 echo "Removing release $RELEASE_NAME"

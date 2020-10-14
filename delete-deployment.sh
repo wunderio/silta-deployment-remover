35c766c38b8056a2ab7ba014bbc73fcc057f7f63
Jānis Bebrītis <janis.bebritis@wunder.io>
2020-10-14 16:55:14 +0300

    Debug cluster login and removal process; silence pod privilege checking

--- delete-deployment.sh
+++ delete-deployment.sh
@@ -1,23 +1,30 @@
 remove_release () {
   # Remove deployment
-  echo "Removing release $RELEASE_NAME"
+  echo "REMOVER: Removing release $NAMESPACE/$RELEASE_NAME"
   helm delete -n "$NAMESPACE" "$RELEASE_NAME"
+  echo "REMOVER: $NAMESPACE/$RELEASE_NAME helm delete status: $?"
 
   # Remove jobs
   kubectl delete job -l release="$RELEASE_NAME" -n "$NAMESPACE"
+  echo "REMOVER: $NAMESPACE/$RELEASE_NAME job delete status: $?"
 
   # Remove PersistentVolumeClaim left over from StatefulSets
   kubectl delete pvc -l release="$RELEASE_NAME" -n "$NAMESPACE"
+  echo "REMOVER: $NAMESPACE/$RELEASE_NAME sts pvc delete status: $?"
 
   # Also remove PersistentVolumeClaims from Elasticsearch, that chart has different labels.
   kubectl delete pvc -l app="${RELEASE_NAME}-es" -n "$NAMESPACE"
+  echo "REMOVER: $NAMESPACE/$RELEASE_NAME es pvc delete status: $?"
+  
+  echo "REMOVER: $NAMESPACE/$RELEASE_NAME release removed"
 }
 
-if ! kubectl auth can-i delete pod ; then
+if [ ! `kubectl auth can-i delete pod` ] ; then
   # Save key, authenticate and set compute zone
   echo $GCLOUD_KEY_JSON > ${HOME}/gcloud-service-key.json
-  echo "Logging on to Google Cloud"
+  echo "REMOVER: Logging on to Google Cloud"
   gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json --project $GCLOUD_PROJECT_NAME
+  echo "REMOVER: gcloud auth status: $?"
   gcloud config set compute/zone $GCLOUD_COMPUTE_ZONE
 
   # Updates a kubeconfig file with appropriate credentials and endpoint information
@@ -25,6 +32,7 @@ if ! kubectl auth can-i delete pod ; then
   gcloud container clusters get-credentials $GCLOUD_CLUSTER_NAME \
     --zone $GCLOUD_COMPUTE_ZONE \
     --project $GCLOUD_PROJECT_NAME
+  echo "REMOVER: gcloud container clusters status: $?"
 fi
 
 # Legacy remover
